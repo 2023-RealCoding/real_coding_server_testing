b4028b6ed230f0be37822478042ac86b5d7d84ca
inspire12 <ox4443@naver.com>
2023-04-16 23:48:52 +0900

    feature(feature/deploy) - profile 적용

--- src/test/java/com/cnu/real_coding_server/service/week1/base/service/PostServiceWithSpringWithMockTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/base/service/PostServiceWithSpringWithMockTest.java
@@ -15,7 +15,9 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class PostServiceWithSpringWithMockTest {
 
