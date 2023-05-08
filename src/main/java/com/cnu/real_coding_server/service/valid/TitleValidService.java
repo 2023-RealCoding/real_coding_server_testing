4ae573ec56ee7b60226addeb96eb6942bef8aa00
wgrgwg <kyoeng14697@naver.com>
2023-05-08 14:34:28 +0900

    202104343

--- src/main/java/com/cnu/real_coding_server/service/valid/TitleValidService.java
+++ src/main/java/com/cnu/real_coding_server/service/valid/TitleValidService.java
@@ -4,19 +4,14 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostValidService {
+public class TitleValidService {
     public boolean isSlangInclude(List<String> slangList,
-                                  String title,
-                                  String postContent) {
+                                  String title) {
         for (String slang : slangList) {
-            if(title.contains(slang)
-                    || postContent.contains(slang)) {
+            if(title.contains(slang)) {
                 return true;
             }
         }
         return false;
     }
 }
-
-
-
--- src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
@@ -2,8 +2,11 @@ package com.cnu.real_coding_server.service.week1.practice.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.cnu.real_coding_server.service.valid.ContentValidService;
 import com.cnu.real_coding_server.service.valid.PostValidService;
 import java.util.List;
+
+import com.cnu.real_coding_server.service.valid.TitleValidService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,10 @@ public class PostValidServiceTest {
 
     @Autowired
     PostValidService postValidService;
+    @Autowired
+    TitleValidService titleValidService;
+    @Autowired
+    ContentValidService contentValidService;
 
 
     @DisplayName("post 제목/본문에 비속어가 있나 테스트")
@@ -28,4 +35,30 @@ public class PostValidServiceTest {
         // then 실행 결과 검증
         assertThat(validPost).isEqualTo(true);
     }
+
+    @DisplayName("post 제목에 비속어가 있나 테스트")
+    @Test
+    void testValidTitleIncludeSlang() {
+        // given 시나리오
+        String testTitle = "비속어가 섞인 제목";
+        List<String> slangList = List.of("비속어", "비속어2");
+        // when 코드 실행
+        boolean validTitle = titleValidService.isSlangInclude(slangList, testTitle);
+        // then 실행 결과 검증
+        assertThat(validTitle).isEqualTo(true);
+    }
+
+    @DisplayName("post 본문에 비속어가 있나 테스트")
+    @Test
+    void testValidContentIncludeSlang() {
+        // given 시나리오
+        String testContent = "비속어가 섞인 욕";
+        List<String> slangList = List.of("비속어", "비속어2");
+        // when 코드 실행
+        boolean validContent = contentValidService.isSlangInclude(slangList, testContent);
+        // then 실행 결과 검증
+        assertThat(validContent).isEqualTo(true);
+    }
+
+
 }
