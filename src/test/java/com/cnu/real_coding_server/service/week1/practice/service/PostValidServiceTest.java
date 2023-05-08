11947b1fc07552d2431a61958432bec2de66303a
minkyung <xmskwo349@naver.com>
2023-05-08 16:53:06 +0900

    pr

--- src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+@ActiveProfiles("dev")
 @SpringBootTest
 public class PostValidServiceTest {
 
@@ -18,15 +18,26 @@ public class PostValidServiceTest {
     PostValidService postValidService;
 
 
-    @DisplayName("post 제목/본문에 비속어가 있나 테스트")
+    @DisplayName("post 제목에 비속어가 있나 테스트")
     @Test
-    void testValidPostIncludeSlang() {
+    void testValidPostIncludeTitle() {
         // given 시나리오
         String testTitle = "비속어가 섞인 제목";
+        List<String> slangList = List.of("비속어", "비속어2");
+
+        boolean validPost = postValidService.isSlangInclude(slangList, testTitle, "");
+        // then 검증
+        assertThat(validPost).isEqualTo(true);
+    }
+
+    @DisplayName("post 본문에 비속어가 있나 테스트")
+    @Test
+    void testValidPostIncludeContent() {
+        // given 시나리오
         String testContent = "비속어가 섞인 욕";
         List<String> slangList = List.of("비속어", "비속어2");
 
-        boolean validPost = postValidService.isSlangInclude(slangList, testTitle, testContent);
+        boolean validPost = postValidService.isSlangInclude(slangList, "", testContent);
         // then 검증
         assertThat(validPost).isEqualTo(true);
     }
