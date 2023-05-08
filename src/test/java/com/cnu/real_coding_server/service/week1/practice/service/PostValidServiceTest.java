c5cef16bceb9b730497d2fdfb73e36adf002dddf
YIMSEBIN <jij09123@o.cnu.ac.kr>
2023-05-08 16:12:32 +0900

    테스팅과제

--- src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
@@ -17,13 +17,25 @@ public class PostValidServiceTest {
     @Autowired
     PostValidService postValidService;
 
-
-    @DisplayName("post 제목/본문에 비속어가 있나 테스트")
+    @DisplayName("post 제목/본문에 비속어가 있나 테스트1")
     @Test
     void testValidPostIncludeSlang() {
         // given 시나리오
         String testTitle = "비속어가 섞인 제목";
         String testContent = "비속어가 섞인 욕";
+        List<String> slangList = List.of("비속어", "비속어2");
+
+        boolean validPost = postValidService.isSlangInclude(slangList, testTitle, testContent);
+        // then 검증
+        assertThat(validPost).isEqualTo(true);
+    }
+
+    @DisplayName("post 제목/본문에 비속어가 있나 테스트2")
+    @Test
+    void testValidPostIncludeSlang2() {
+        // given 시나리오
+        String testTitle = "비속어2 제목";
+        String testContent = "비속어2 욕";
         List<String> slangList = List.of("비속어", "비속어2");
 
         boolean validPost = postValidService.isSlangInclude(slangList, testTitle, testContent);
