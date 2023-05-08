f44a0c82727fa08a7d01bd59696609d50c6ae3e7
duswnwn <rita27@naver.com>
2023-05-08 20:17:08 +0900

    real_coding

--- src/main/java/com/cnu/real_coding_server/service/PostService.java
+++ src/main/java/com/cnu/real_coding_server/service/PostService.java
@@ -1,33 +1,45 @@
 package com.cnu.real_coding_server.service;
 
 import com.cnu.real_coding_server.entity.Post;
+import com.cnu.real_coding_server.error.SlangBadRequestException;
 import com.cnu.real_coding_server.model.request.PostRequest;
 import com.cnu.real_coding_server.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
+import com.cnu.real_coding_server.service.valid.PostValidService;
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+    private static final List<String> slangList = List.of("비속어1", "비속어2");
+    private final PostValidService postValidService;
 
     public Post createPost(PostRequest postRequest) {
+
+        if (postValidService.isSlangInclude(slangList, postRequest.getTitle(), postRequest.getContents())) {
+            throw new SlangBadRequestException();
+        }
+        log.info("정상 저장 확인");
         return postRepository.save(postRequest.toEntity());
     }
 
     public List<Post> getPosts() {
         return postRepository.findAll();
     }
-
     public Optional<Post> getPost(Integer postId) {
         return postRepository.findById(postId);
     }
 
     public Optional<Post> updatePost(Integer postId, PostRequest postRequest) {
+        if (postValidService.isSlangInclude(slangList, postRequest.getTitle(), postRequest.getContents())) {
+            throw new SlangBadRequestException();
+        }
         return postRepository.findById(postId)
                 .map(post -> {
                     post.setTitle(postRequest.getTitle());
@@ -36,7 +48,6 @@ public class PostService {
                     return postRepository.save(post);
                 });
     }
-
     public void deletePost(Integer postId) {
         postRepository.findById(postId)
                 .ifPresent(postRepository::delete);
