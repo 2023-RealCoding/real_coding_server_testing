90639fee524aa656840b3303872f131e9f4a3e4e
UseHashMap <127710446+UseHashMap@users.noreply.github.com>
2023-05-11 23:28:43 +0900

    homework

--- src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
@@ -8,16 +8,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 public class PostValidServiceTest {
 
     @Autowired
     PostValidService postValidService;
 
-
+    /*
     @DisplayName("post 제목/본문에 비속어가 있나 테스트")
     @Test
     void testValidPostIncludeSlang() {
@@ -29,5 +27,24 @@ public class PostValidServiceTest {
         boolean validPost = postValidService.isSlangInclude(slangList, testTitle, testContent);
         // then 검증
         assertThat(validPost).isEqualTo(true);
+    }
+    */
+    @DisplayName("post 제목에 비속어가 있나 테스트")
+    @Test
+    void testValidPostIncludeSlangInTitle() {
+        // given
+        String testTitle = "비속어가 섞인 제목";
+        List<String> slangList = List.of("비속어", "비속어같지만일만어");
+        // when postValidService 에 제목에 비속어가 있는지 확인하는 함수를 만들어 실행 결과 반환
+        // then 결과가 true인지 확인
+    }
+
+    @DisplayName("post 본문에 비속어가 있나 테스트")
+    @Test
+    void testValidPostIncludeSlangInContent() {
+        String testContent = "비속어가 섞인 제목";
+        List<String> slangList = List.of("비속어", "비속어같지만일만어");
+        // when postValidService 에 본문에 비속어가 있는지 확인하는 함수를 만들어 실행 결과 반환
+        // then 결과가 true인지 확인
     }
 }
