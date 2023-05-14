261e9ec8463871470649cb034269c5f5bb1459bd
LeeHeeSung98989898 <107474367+LeeHeeSung98989898@users.noreply.github.com>
2023-05-14 18:31:32 +0900

    Update PostValidServiceTest.java homework

--- src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/practice/service/PostValidServiceTest.java
@@ -18,14 +18,35 @@ public class PostValidServiceTest {
     PostValidService postValidService;
 
 
-    @DisplayName("post 제목/본문에 비속어가 있나 테스트")
+//     @DisplayName("post 제목/본문에 비속어가 있나 테스트")
+//     @Test
+//     void testValidPostIncludeSlang() {
+//         // given 시나리오
+//         String testTitle = "비속어가 섞인 제목";
+//         String testContent = "비속어가 섞인 욕";
+//         List<String> slangList = List.of("비속어", "비속어2");
+
+//         boolean validPost = postValidService.isSlangInclude(slangList, testTitle, testContent);
+//         // then 검증
+//         assertThat(validPost).isEqualTo(true);
+//     }
+    @DisplayName("post 제목에 비속어가 있나 테스트")
     @Test
-    void testValidPostIncludeSlang() {
-        // given 시나리오
+    void testValidPostIncludeSlangInTitle() {
         String testTitle = "비속어가 섞인 제목";
         String testContent = "비속어가 섞인 욕";
         List<String> slangList = List.of("비속어", "비속어2");
+        boolean validPost = postValidService.isSlangInclude(slangList, testTitle, testContent);
+        // then 검증
+        assertThat(validPost).isEqualTo(true);
+    }
 
+    @DisplayName("post 본문에 비속어가 있나 테스트")
+    @Test
+    void testValidPostIncludeSlangInContent() {
+        String testTitle = "비속어가 섞인 본문";
+        String testContent = "비속어가 섞인 욕";
+        List<String> slangList = List.of("비속어", "비속어2");
         boolean validPost = postValidService.isSlangInclude(slangList, testTitle, testContent);
         // then 검증
         assertThat(validPost).isEqualTo(true);
